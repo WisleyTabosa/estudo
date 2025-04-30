05d9b7604085f567b3ce3a0df6d85cd0edea8bcf
Wisley Tabosa <wtabosas@gmail.com>
2025-04-30 14:12:50 -0300

    menssagem

---
+++ javascript/aula03/java.js
@@ -0,0 +1,5 @@
+let numero1 = Number(prompt("Digite o primeiro numero"));
+let numero2 = Number(prompt("Digite o segundo numero"));
+let resultado = numero1 + numero2;
+document.writeln('O Resultado é:' + resultado)
+console.log(resultado);
